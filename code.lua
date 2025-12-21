829ebd1472b5ac33c616893ba4393f78548c9dbe
İbrahim ÇAPAR <200311085@ogr.cbu.edu.tr>
2025-12-21 03:03:59 +0300

    Categories görünümü güncellendi

--- code.lua
+++ code.lua
@@ -1,38 +1,27 @@
 function CodeBlock(el)
-  if #el.classes > 0 then
-    local lang = el.classes[1]
-    
+  if el.classes:includes("cell-code") then return nil end
 
-    local title = el.attributes['title']
-    
-    if not title then
-      title = el.attributes['filename']
-    end
-    
-    if not title then
-      title = lang:upper() .. " Kod Bloğu"
-    end
-    
-    local hasSourceCode = false
-    for _, cls in ipairs(el.classes) do
-      if cls == "sourceCode" then
-        hasSourceCode = true
-        break
-      end
-    end
-    
-    if not hasSourceCode then
-      table.insert(el.classes, "sourceCode")
-    end
-    
-    return quarto.Callout({
-      type = "note",
-      content = { el },
-      title = title,
-      collapse = true,
-      icon = false,
-      appearance = "simple"
-    })
+  local lang = el.classes[1]
+  
+  local title = el.attributes['title']
+  if not title then title = el.attributes['filename'] end
+  if not title and lang then title = lang:upper() .. " Kod Bloğu" end
+  if not title then title = "Kod Bloğu" end 
+
+  if not el.classes:includes("sourceCode") then
+    el.classes:insert("sourceCode")
   end
-  return nil
+
+  if not el.classes:includes("number-lines") then
+    el.classes:insert("number-lines")
+  end
+  
+  return quarto.Callout({
+    type = "note",
+    content = { el }, 
+    title = title,
+    collapse = true,
+    icon = false,
+    appearance = "simple" 
+  })
 end
