0414b689cdbf6a1c26c976e71547032557819211
İbrahim ÇAPAR <200311085@ogr.cbu.edu.tr>
2025-12-22 17:01:41 +0300

    Code View

--- code.lua
+++ code.lua
@@ -1,27 +1,27 @@
 function CodeBlock(el)
-  if el.classes:includes("cell-code") then return nil end
+  if #el.classes > 0 then
+    local lang = el.classes[1]
+    
+    local title = el.attributes['title']
+    if not title then title = el.attributes['filename'] end
+    if not title then title = lang:upper() .. " Kod Bloğu" end
+    
+    if not el.classes:includes('sourceCode') then
+      table.insert(el.classes, 'sourceCode')
+    end
 
-  local lang = el.classes[1]
-  
-  local title = el.attributes['title']
-  if not title then title = el.attributes['filename'] end
-  if not title and lang then title = lang:upper() .. " Kod Bloğu" end
-  if not title then title = "Kod Bloğu" end 
-
-  if not el.classes:includes("sourceCode") then
-    el.classes:insert("sourceCode")
+    if not el.classes:includes('number-lines') then
+      table.insert(el.classes, 'number-lines')
+    end
+    
+    return quarto.Callout({
+      type = "note",
+      content = { el },
+      title = title,
+      collapse = true,
+      icon = false,
+      appearance = "simple" 
+    })
   end
-
-  if not el.classes:includes("number-lines") then
-    el.classes:insert("number-lines")
-  end
-  
-  return quarto.Callout({
-    type = "note",
-    content = { el }, 
-    title = title,
-    collapse = true,
-    icon = false,
-    appearance = "simple" 
-  })
+  return nil
 end
